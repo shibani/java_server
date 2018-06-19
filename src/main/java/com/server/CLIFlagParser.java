5fa2d55a138df66709a19bec500d1dfa1fc9424c
shibani <shibani@8thlight.com>
2018-06-19 17:07:41 -0400

    built out cli flag parser

--- src/main/java/com/server/CLIFlagParser.java
+++ src/main/java/com/server/CLIFlagParser.java
@@ -1,23 +1,27 @@
 package com.server;
 
+import java.io.IOException;
+
 public class CLIFlagParser {
 
-    public ServerConfig parse(String args){
+    public ServerConfig parse(String args) throws IOException {
         String[] splitArgs = args.split(" ");
         int portNumber;
         String directory;
 
-        if(splitArgs[0].equals("-p")){
-            portNumber = Integer.parseInt(splitArgs[1]);
-            directory = splitArgs[3];
-        } else {
-            portNumber = Integer.parseInt(splitArgs[3]);
-            directory = splitArgs[1];
+        try{
+            if(splitArgs[0].equals("-p") && splitArgs[2].equals("-d")){
+                portNumber = Integer.parseInt(splitArgs[1]);
+                directory = splitArgs[3];
+            } else if(splitArgs[0].equals("-d") && splitArgs[2].equals("-p")){
+                portNumber = Integer.parseInt(splitArgs[3]);
+                directory = splitArgs[1];
+            } else {
+                throw new IOException();
+            }
+        } catch (ArrayIndexOutOfBoundsException e) {
+            throw new IOException();
         }
-        //do regex
-        //create serverconfig object
-        //return serverconfig object
-        //handle errors
         return new ServerConfig(portNumber, directory);
     }
 
--- src/test/java/com/server/CLIFlagParserTest.java
+++ src/test/java/com/server/CLIFlagParserTest.java
@@ -2,12 +2,14 @@ package com.server;
 
 import org.junit.Test;
 
+import java.io.IOException;
+
 import static org.junit.Assert.*;
 
 public class CLIFlagParserTest {
 
     @Test
-    public void successful_parse() {
+    public void successful_parse() throws IOException {
         String args = "-p 5000 -d /path/to/directory";
 
         final CLIFlagParser parser = new CLIFlagParser();
@@ -18,7 +20,7 @@ public class CLIFlagParserTest {
     }
 
     @Test
-    public void successful_parse_with_swapped_flags() {
+    public void successful_parse_with_swapped_flags() throws IOException {
         String args = "-d /path/to/directory -p 5000";
 
         final CLIFlagParser parser = new CLIFlagParser();
@@ -28,4 +30,28 @@ public class CLIFlagParserTest {
         assertEquals("/path/to/directory", serverConfig.getDirectory());
     }
 
+    @Test(expected = IOException.class)
+    public void failed_parse_with_swapped_flags() throws IOException {
+        String args = "-f /path/to/directory -p 5000";
+
+        final CLIFlagParser parser = new CLIFlagParser();
+        parser.parse(args);
+    }
+
+
+    @Test(expected = IOException.class)
+    public void failed_parse_with_missing_portnumber() throws IOException {
+        String args = "-d /path/to/directory";
+
+        final CLIFlagParser parser = new CLIFlagParser();
+        parser.parse(args);
+    }
+
+    @Test(expected = IOException.class)
+    public void failed_parse_with_missing_directory() throws IOException {
+        String args = "-p 5000";
+
+        final CLIFlagParser parser = new CLIFlagParser();
+        parser.parse(args);
+    }
 }
