4df3fc62a259a33badd0e5f63ecabe5fc2e6f3c1
Thomas Countz <thomascountz@gmail.com>
2018-06-27 11:29:14 -0400

    initial request line parsing

--- src/main/java/com/server/RequestHeaderParser.java
+++ src/main/java/com/server/RequestHeaderParser.java
@@ -1,4 +1,34 @@
 package com.server;
 
 public class RequestHeaderParser {
+
+    private String headerString;
+    private String verb;
+    private String route;
+
+    RequestHeaderParser(String headerString){
+        this.headerString = headerString;
+        extractVerb();
+        extractRoute();
+    }
+
+    private void extractVerb() {
+        String requestLine = headerString.split("\n")[0];
+        String verb = requestLine.split(" ")[0];
+        this.verb = verb.trim();
+    }
+
+    private void extractRoute() {
+        String requestLine = headerString.split("\n")[0];
+        String route = requestLine.split(" ")[1];
+        this.route = route.trim();
+    }
+
+    public String getVerb(){
+        return this.verb;
+    }
+
+    public String getRoute(){
+        return this.route;
+    }
 }
