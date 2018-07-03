4dbd91bca3b7151a4020884853d5728ca40684f5
Thomas Countz <thomascountz@gmail.com>
2018-07-03 10:18:46 -0400

    remove commented-out tests from RequestRouterTest

--- src/test/java/com/server/RequestRouterTest.java
+++ src/test/java/com/server/RequestRouterTest.java
@@ -1,29 +1,10 @@
 package com.server;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class RequestRouterTest {
-
-//    @Test
-//    public void anyWhiteListedPathReturnsTrue(){
-//
-//        RequestRouter requestRouter = new RequestRouter();
-//        boolean result = requestRouter.checkPath("/form");
-//
-//        assertTrue(result);
-//    }
-//
-//    @Test
-//    public void anyNonWhiteListedPathReturnFalse(){
-//
-//        RequestRouter requestRouter = new RequestRouter();
-//        boolean result = requestRouter.checkPath("/foobar");
-//
-//        assertFalse(result);
-//    }
 
     @Test
     public void getResponseCodeReturnsAResponseCode(){
